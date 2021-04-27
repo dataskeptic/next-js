1306f1825a0a496b24445f2498662f84e7f85bb7
fullpistoladev <mateus_henrique2011@outlook.com>
2021-04-26 21:34:38 -0300

    style layout

---
+++ pages/about.js
@@ -0,0 +1,17 @@
+import Head from "next/head"
+
+const about = () => {
+    return (
+        <div>
+
+            <Head>
+                <title>About</title>
+                <meta name='keywords' content='web development, programing'></meta>
+            </Head>
+
+            <h1>About</h1>
+        </div>
+    )
+}
+
+export default about
